844eb867dc17e3de2a4f377b06259ca9e9a781f2
roboslyq <robosloveu@163.com>
2018-09-14 10:00:00 +0800

    jdk 1.8源码

--- src/roboslyq/Test.java
+++ src/roboslyq/Test.java
@@ -11,9 +11,8 @@ public class Test {
     private static final int STOP       =  1 << COUNT_BITS;
     private static final int TIDYING    =  2 << COUNT_BITS;
     private static final int TERMINATED =  3 << COUNT_BITS;
+
     public static void main(String[] args) {
-
-
         System.out.println(COUNT_BITS);
         System.out.println(CAPACITY);
         System.out.println(RUNNING);
